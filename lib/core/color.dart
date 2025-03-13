878b79be753a5252ce1a7d5f0bb7286536cf3bf2
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-13 08:33:19 +0500

    Update color scheme and add new dependency
    - Changed main color and updated shades of pink for a refreshed look
    - Introduced new accent colors and background color for enhanced UI
    - Added flutter_advanced_drawer dependency to pubspec.yaml for improved navigation

--- lib/core/color.dart
+++ lib/core/color.dart
@@ -1,15 +1,18 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-const Color mainColor = Color(0xFFFE4678);
-const Color pink01 = Color(0xFFFEE3EA);
-const Color pink02 = Color(0xFFFEEBF0);
-const Color pink03 = Color(0xFFFEE0E8);
+const Color mainColor = Color(0xFF6B4B3E);
+const Color pink01 = Color(0xFFFFF0E9);
+const Color pink02 = Color(0xFFFFF5EE);
+const Color pink03 = Color(0xFFFFE4D9);
 
-const Color gray02 = Color(0XFFE0E0E0);
+const Color gray02 = Color(0xFFE0E0E0);
 const Color blackShadow = Color(0xFFDAD9DA);
-const Color grayColor = Color(0XFFA3A3A3);
-const Color black = Color(0XFF414141);
-
+const Color grayColor = Color(0xFFA3A3A3);
+const Color black = Color(0xFF414141);
 const Color white = Color(0xFFFFFFFF);
-
 const Color ratingC = Color(0xFFFF972F);
+
+// New colors
+const Color accent1 = Color(0xFFD4A373);
+const Color accent2 = Color(0xFFFAEDCD);
+const Color background = Color(0xFFFEFAE0);
